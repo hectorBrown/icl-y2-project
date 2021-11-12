fixed scaling issue and improved matplotlib code

graphics.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 """
 Graphics utility functions.
+
+A bug in some versions of matplotlib will prevent these functions generating useful output for small values.
+Update matplotlib to fix -- please see https://github.com/matplotlib/matplotlib/issues/6015.
 """
 
 import matplotlib.pyplot as plt, numpy as np
@@ -16,8 +19,12 @@ def graph_zplane(rays, z):
 
     #check if there are any at all
     if len(xy):
-        fig = plt.figure()
-        plt.scatter(*np.array(xy).transpose())
+        fig, ax = plt.subplots()
+        ax.scatter(*np.array(xy).transpose())
+        #attempted fix for matplotlib bug
+        ax.set_xlim(-abs(max([x[0] for x in xy])) * 1.1, abs(max([x[0] for x in xy])) * 1.1)
+        ax.set_ylim(-abs(max([x[1] for x in xy])) * 1.1, abs(max([x[1] for x in xy])) * 1.1)
+        ax.set_aspect("equal")
         return fig
     else:
         raise Exception("None of the rays have positions for this z value.")
@@ -27,7 +34,7 @@ def graph_yplane(rays):
     Graphs a set of rays as a y-z plane.
     """
 
-    fig = plt.figure()
+    fig, ax = plt.subplots()
     for ray in rays:
-        plt.plot([x[2] for x in ray.vertices()], [x[1] for x in ray.vertices()])
+        ax.plot([x[2] for x in ray.vertices()], [x[1] for x in ray.vertices()])
     return fig