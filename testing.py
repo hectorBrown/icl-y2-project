d08033fce561a0e7497f77599f36f57fbddc99ad
Hector Brown <hectorjbrown@protonmail.com>
2021-12-06 01:15:58 +0000

    update tests to System syntax

--- testing.py
+++ testing.py
@@ -7,61 +7,56 @@ import numpy as np, matplotlib.pyplot as plt
 import elements as e, graphics as g, opticsutils as ou, ray as r, optimizer as ot
 
 def t9():
-    refractor = e.SphericalRefractor(100e-3, 0.03e3, 1, 1.5)
-    output = e.OutputPlane(250e-3)
+    sys = e.System()
+    sys.append(e.SphericalRefractor(100e-3, 0.03e3, 1, 1.5))
+    sys.append(e.OutputPlane(250e-3))
     bundle = [r.Ray([0, (x / 10) * 1e-3, 0], [0,0,1]) for x in range(25)]
     
-    for ray in bundle:
-        refractor.propagate(ray)
-        output.propagate(ray)
+    sys.propagate(bundle)
     
     return (g.graph_yplane(bundle), g.graph_zplane(bundle, 250e-3))
 
 def t10():
-    refractor = e.SphericalRefractor(100e-3, 0.03e3, 1, 1.5)
-    output = e.OutputPlane(250e-3)
+    sys = e.System()
+    sys.append(e.SphericalRefractor(100e-3, 0.03e3, 1, 1.5))
+    sys.append(e.OutputPlane(250e-3))
     bundle = [r.Ray([0,0,0], [0,0,1]), r.Ray([0,0.1e-3,0], [0,0,1])]
     
-    for ray in bundle:
-        refractor.propagate(ray)
-        output.propagate(ray)
+    sys.propagate(bundle)
     
     return g.graph_yplane(bundle)
 
 def t11():
-    refractor = e.SphericalRefractor(100e-3, 0.03e3, 1, 1.5)
-    output = e.OutputPlane(400e-3)
+    sys = e.System()
+    sys.append(e.SphericalRefractor(100e-3, 0.03e3, 1, 1.5))
+    sys.append(e.OutputPlane(400e-3))
     bundle = [r.Ray([0, 2e-3, 0], [0,0,1]),
               r.Ray([0, 2e-3, 0], [0, -2e-3, 100e-3]),
               r.Ray([0, 2e-3, 0], [0, -4e-3, 100e-3])]
     
-    for ray in bundle:
-        refractor.propagate(ray)
-        output.propagate(ray)
+    sys.propagate(bundle)
     
     return (g.graph_yplane(bundle), g.graph_zplane(bundle, 400e-3))
 
 def t12():
-    refractor = e.SphericalRefractor(100e-3, 0.03e3, 1, 1.5)
-    output = e.OutputPlane(250e-3)
+    sys = e.System()
+    sys.append(e.SphericalRefractor(100e-3, 0.03e3, 1, 1.5))
+    sys.append(e.OutputPlane(250e-3))
     bundle = r.bundle(5e-3, 6, 6)
     
-    for ray in bundle:
-        refractor.propagate(ray)
-        output.propagate(ray)
+    sys.propagate(bundle)
     
     #focal length = nR/(n-1)
     
     return (g.graph_yplane(bundle), g.graph_zplane(bundle, 250e-3))
 
 def t13():
-    refractor = e.SphericalRefractor(100e-3, 0.03e3, 1, 1.5)
-    output = e.OutputPlane(250e-3)
+    sys = e.System()
+    sys.append(e.SphericalRefractor(100e-3, 0.03e3, 1, 1.5))
+    sys.append(e.OutputPlane(250e-3))
     bundle = r.bundle(5e-3, 6, 6)
     
-    for ray in bundle:
-        refractor.propagate(ray)
-        output.propagate(ray)
+    sys.propagate(bundle)
         
     spots = [np.linalg.norm(r.get_xy(200e-3))**2 for r in bundle]
     
@@ -95,27 +90,26 @@ def optimization_ext():
 def chromatic_ext():
     def index_func(wavelength):
         return 1.5168 + (wavelength - 380e-9) / (740e-9 - 380e-9) * 0.001
-    lens = [e.SphericalRefractor(100e-3, 0.02e3, 1, index_func),
-            e.SphericalRefractor(105e-3, 0, index_func, 1)]
-    output = e.OutputPlane(250e-3)
+    sys = e.System()
+    sys.append(e.SphericalRefractor(100e-3, 0.02e3, 1, index_func))
+    sys.append(e.SphericalRefractor(105e-3, 0, index_func, 1))
+    sys.append(e.OutputPlane(250e-3))
     red = r.bundle(10e-3, 6, 6, wavelength=380e-9)
     green = r.bundle(10e-3, 6, 6, wavelength=(740 + 380) / 2 * 1e-9)
     blue = r.bundle(10e-3, 6, 6, wavelength=740e-9)
     bundle = np.array(list(red) + list(green) + list(blue))
-    for ray in bundle:
-        for surface in lens:
-            surface.propagate(ray)
-        output.propagate(ray)
     
+    sys.propagate(bundle)
+
     return g.graph_zplane(bundle, 200e-3)
 
 def reflecting_ext():
-    mirror = e.SphericalReflector(100e-3, -0.02e3)
+    sys = e.System()
+    sys.append(e.SphericalReflector(100e-3, -0.02e3))
+    sys.append(e.OutputPlane(-50e-3))
     bundle = r.bundle(10e-3, 6, 6)
-    output = e.OutputPlane(-50e-3)
-    for ray in bundle:
-        mirror.propagate(ray)
-        output.propagate(ray)
+
+    sys.propagate(bundle)
 
     return g.graph_yplane(bundle)
 
